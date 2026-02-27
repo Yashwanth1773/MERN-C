beb24f351d2c81e7e8ab8d02e8bf75e7cfa13fee
Yash <tangellayashwanth@gmail.com>
2026-02-27 16:05:38 +0530

    basic javascript

---
+++ javascript/script.js
@@ -0,0 +1,42 @@
+console.log("Hello world")
+// //reinitialize and redeclare
+// var a;
+// a=10;
+// var a;
+// a=20;
+// //reinitialize
+// let b=10;
+// //let b;
+// b=99;
+// //canot redeclare or reinitialize
+// const c=22;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+var num=10;
+var str="yash";
+var bool=true;
+var un;
+var nu=null;
+var big=488748387439n;
+var symbol=Symbol('li');
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof un);
+console.log(typeof nu);
+console.log(typeof big);
+console.log(typeof symbol);
+
+var arr=[1,2,3,4,5]
+console.log(typeof arr);
+console.log(arr);
+
+var obj={
+    name:"yash",
+    dept:"DS"
+}
+console.log(obj);
+console.log(typeof obj);
+
